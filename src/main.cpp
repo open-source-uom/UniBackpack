6d4701df61bf07f34569e7de8621af2727881263
Apostolos Chalis <tolishalis@gmail.com>
2026-05-16 16:43:21 +0300

    Logo now shows as app icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <QApplication>
 #include "MainWindow.hpp"
 
-int main(int argc, char *argv[]){ 
+int main(int argc, char *argv[]){
 	QApplication uni_backpack_app(argc, argv);
+	uni_backpack_app.setWindowIcon(QIcon(":/icons/unibackpack.png"));
+	uni_backpack_app.setDesktopFileName("UniBackpack");
 
 	MainWindow main_window;
 	main_window.show();
-	
+
 	return uni_backpack_app.exec();
 }
